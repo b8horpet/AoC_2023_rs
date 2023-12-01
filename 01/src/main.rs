896444cbb51a9b72687c3fee385c109ae45c2c20
b8horpet <b8horpet@gmail.com>
2023-12-01 20:54:52 +0100

    day1 star2

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,8 +1,21 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 fn main() -> std::io::Result<()> {
+    let digits : HashMap<i32, Vec<&str>> = HashMap::from([
+        (0, vec!["0", "zero"]),
+        (1, vec!["1", "one"]),
+        (2, vec!["2", "two"]),
+        (3, vec!["3", "three"]),
+        (4, vec!["4", "four"]),
+        (5, vec!["5", "five"]),
+        (6, vec!["6", "six"]),
+        (7, vec!["7", "seven"]),
+        (8, vec!["8", "eight"]),
+        (9, vec!["9", "nine"]),
+    ]);
     let args: Vec<String> = env::args().collect();
     let filename = {
         if args.len() > 1 { &args[1][..] }
@@ -10,26 +23,50 @@ fn main() -> std::io::Result<()> {
     };
     let mut file = File::open(filename)?;
     let mut contents = String::new();
+    let mut result1 = 0_usize;
+    let mut result2 = 0_usize;
     file.read_to_string(&mut contents)?;
-    let mut first_num = -1;
-    let mut last_num = -1;
-    let mut result = 0;
-    for c in contents.chars() {
-        match c {
-            '\n' => {
-                assert!((0..=9).contains(&first_num));
-                assert!((0..=9).contains(&last_num));
-                result += first_num * 10 + last_num;
-                first_num = -1;
-                last_num = -1;
+    for l in contents.lines() {
+        let mut first_val1 = (-1,l.len());
+        let mut last_val1 = (-1, 0_usize);
+        let mut first_val2 = (-1,l.len());
+        let mut last_val2 = (-1, 0_usize);
+        for (v,ds) in &digits {
+            assert!(!ds.is_empty());
+            // star1
+            let d1 = ds[0];
+            if let Some(p) = l.find(d1) {
+                if p <= first_val1.1 {
+                    first_val1 = (*v,p);
+                }
             }
-            '0' ..= '9' => {
-                last_num = (c as i32) - ('0' as i32);
-                if first_num == -1 { first_num = last_num; }
+            if let Some(p) = l.rfind(d1) {
+                    if p >= last_val1.1 {
+                    last_val1 = (*v,p);
+                }
             }
-            _ => {}
+            for d in ds {
+                if let Some(p) = l.find(d) {
+                    if p <= first_val2.1 {
+                        first_val2 = (*v,p);
+                    }
+                }
+                if let Some(p) = l.rfind(d) {
+                        if p >= last_val2.1 {
+                        last_val2 = (*v,p);
+                    }
+                }
+            }
         }
+        assert!((0..=9).contains(&first_val1.0));
+        assert!((0..=9).contains(&last_val1.0));
+        assert!((0..=9).contains(&first_val2.0));
+        assert!((0..=9).contains(&last_val2.0));
+        result1 += (first_val1.0 * 10 + last_val1.0) as usize;
+        result2 += (first_val2.0 * 10 + last_val2.0) as usize;
+    
     }
-    println!("{result}");
+    println!("star1: {result1}");
+    println!("star2: {result2}");
     Ok(())
 }
