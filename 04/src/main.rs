4b1439f05ee2ea832b867352a311d310db6118f3
b8horpet <b8horpet@gmail.com>
2023-12-10 14:31:30 +0100

    day4 star2

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -1,14 +1,21 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::HashSet;
+use std::collections::{
+    HashSet,
+    HashMap,
+};
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let mut sum = 0_usize;
-    for l in contents.lines() {
+    let mut copies: HashMap<usize,usize> = HashMap::new();
+    for (id,l) in contents.lines().enumerate() {
         assert!(&l[0..5] == "Card ");
+        let current = copies.entry(id).or_insert(0_usize);
+        *current += 1_usize;
+        let current: usize = *current;
         let card: &str = {
             let v: Vec<&str> = l.split(':').collect();
             assert!(v.len()==2);
@@ -21,17 +28,21 @@ fn main() -> std::io::Result<()> {
         for n in &nums[0] {
             eprint!(" {n}");
         }
-        eprint!(" | darw:");
+        eprint!(" | draw:");
         for n in &nums[1] {
             eprint!(" {n}");
         }
         let i : u32 = nums[0].intersection(&nums[1]).count().try_into().unwrap();
-        eprint!(" => found {i} mathc(es)\n");
+        eprintln!(" => found {i} match(es)");
+        for won in id+1_usize ..= id+(i as usize) {
+            *copies.entry(won).or_insert(0_usize) += current;
+        }
         if i>0u32 {
             sum += 1_usize.checked_shl(i-1u32).unwrap();
         }
     }
     println!("{sum}");
+    println!("{}", copies.iter().fold(0_usize, |a, e| a+e.1));
     Ok(())
 }
 
